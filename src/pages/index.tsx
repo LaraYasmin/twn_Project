9d5cb2854699aa7263ca5f35744ed2b4719c7d87
LaraYasmin <lyasminoliveira18@gmail.com>
2024-06-21 17:34:12 -0300

    Update with the render of data and click to id page ( + design)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from "next";
+import Head from "next/head";
+import router from "next/router";
+import "tailwindcss/tailwind.css";
 
 // defining interfaces
 export interface Props {
@@ -12,11 +15,55 @@ export interface DataProps {
   data: Props[];
 }
 
+export default function Home({ data }: DataProps) {
+  return (
+    <>
+      <Head>
+        <title>TWN Project</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="py-8 text-2xl font-semibold text-center text-white bg-black bg-cover">
+        Posts from the website
+      </div>
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <div className="overflow-x-auto m-16">
+          <table className="min-w-full bg-white shadow-md rounded-xl">
+            <thead className="text-black">
+              <tr>
+                <th className="py-3 px-4 text-left">Id</th>
+                <th className="py-3 px-4 text-center">Title</th>
+                <th className="py-3 px-4 text-left">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="text-blue-gray-900">
+              {data.map((post) => (
+                <tr key={post.id} className="border-b border-blue-gray-200">
+                  <td className="py-3 px-4">{post.id}</td>
+                  <td className="py-3 px-4">{post.title}</td>
+                  <td className="py-3 px-4">
+                    <button
+                      className="font-medium text-blue-600 hover:text-black"
+                      onClick={() => {
+                        router.push(`/posts/${post.id}`);
+                      }}
+                    >
+                      Details
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
 //connection to the api with the return of the data saved
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
 
   return {
--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import "tailwindcss/tailwind.css";
 
 //defining the interface based on the json of the api
 export interface Props {
@@ -11,6 +12,29 @@ export interface Props {
 interface DataProps {
   data: Props;
 }
+
+const PostDetails = ({ data }: DataProps) => {
+  return (
+    <div className="flex items-center justify-center min-h-screen to-stone-900 bg-gradient-to-br bg-gray-100">
+      <div className="w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl">
+        <div className="max-w-md mx-auto space-y-6">
+          <h2 className="text-2xl font-bold"> Post {data.id}</h2>
+          <div className="text-base leading-3">
+            <p className="leading-tight font-normal text-gray-700 m-4">
+              <span className="font-bold">Title: </span>
+              {data.title}
+            </p>
+            <p className="leading-tight font-normal text-gray-700 m-4">
+              <span className="font-bold">Description:</span> {data.body}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDetails;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
